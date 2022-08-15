4a46058df5bbd2679cdd51700136d286cae35d7f
jason06286 <dj4871114@gmail.com>
2022-08-15 16:02:23 +0800

    add: classData.json

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,4 +1,15 @@
-<script setup></script>
+<script setup>
+const getData = async () => {
+  const res = await fetch("../../classData.json");
+  const data = await res.json();
+  console.log("res :>> ", res);
+  console.log("data :>> ", data);
+};
+
+onMounted(() => {
+  getData();
+});
+</script>
 <template>
   <div class="font-serif">
     <Navbar />
