8fe1e86bbdd31af3e945cbcb53055c8091caec67
jason06286 <dj4871114@gmail.com>
2022-08-15 14:57:18 +0800

    chore: env init

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+<template></template>
+<style>
+body {
+  background-color: rgba(203, 211, 219, 1);
+}
+</style>
---
+++ vite.config.js
@@ -0,0 +1,29 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    // ...
+    vue(),
+    AutoImport({
+      imports: ["vue", "vue-router", "vue-i18n"],
+      dts: "src/auto-imports.js",
+    }),
+    Components({
+      // 從 `./src/components/` 路徑查找
+      extensions: ["vue"],
+      include: [/\.vue$/, /\.vue\?vue/],
+      dts: "src/auto-components.js",
+    }),
+  ],
+
+  resolve: {
+    alias: {
+      "@/": `${path.resolve(__dirname, "src")}/`,
+    },
+  },
+});
